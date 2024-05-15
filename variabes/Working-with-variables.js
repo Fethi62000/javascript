42805941dec8cb6e0e0fc8343a666abccd765c34
Fethisensei <fetam62@gmail.com>
2024-05-15 11:05:23 +0200

    feat(varialbles/working-with-variables.js):creat variable

---
+++ variabes/Working-with-variables.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* 
+    Declare two variables: admin and name.
+    Assign the value "John" to name.
+    Copy the value from name to admin.
+    Show the value of admin using alert (must output “John”).
+
+*/
+
+
+
+let admin;
+
+let name = "john";
+
+admin = name;
+
+console.log(admin)
+
+
+
+
+
+
+
+
+
+
+
+
