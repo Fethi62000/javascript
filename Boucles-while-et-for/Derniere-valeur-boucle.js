315ee786f13ec8a827bf6b1bf7d7dfeb88db41ae
Fethisensei <fetam62@gmail.com>
2024-05-15 22:41:10 +0200

    refactor(Dernière-valeur-boucle.js): add loop

---
+++ Boucles-while-et-for/Derniere-valeur-boucle.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+Quelle est la dernière valeur affichée par ce code ? Pourquoi ?
+
+let i = 3;
+
+while (i) {
+  alert( i-- );
+} 
+*/
+
+let i = 3;
+
+while (i) {
+    alert(i--);
+}
+
+// la dernière valeur affichée est 0, car la condition devient fausse et la boucle s'arrêt!
+
