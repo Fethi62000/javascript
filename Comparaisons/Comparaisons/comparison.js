8c981837b5c7bd31588e973a4ff59ba4b676436b
Fethisensei <fetam62@gmail.com>
2024-05-15 13:01:32 +0200

    refactor(comparison.js): add comparisons between differents types

--- Comparaisons/Comparaisons/comparison.js
+++ Comparaisons/Comparaisons/comparison.js
@@ -1,2 +1,22 @@
 "use strict"
 
+/*
+Quel sera le résultat pour les expressions suivantes :
+
+5 > 4
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+*/
+
+5 > 4 // True 
+"apple" > "pineapple" // False 
+"2" > "12" // True
+undefined == null // True
+undefined === null // False
+null == "\n0\n" // False
+null === +"\n0\n" // False
+
