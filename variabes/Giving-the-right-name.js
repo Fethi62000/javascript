421a17ccccf7911699117e36c4c15d7f1b851c68
Fethisensei <fetam62@gmail.com>
2024-05-15 11:17:27 +0200

    refactor(Giving-the-right-name.js):add variables in folder

---
+++ variabes/Giving-the-right-name.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/* 
+    Créez la variable avec le nom de notre planète. Comment nommeriez-vous une telle variable ?
+    Créez la variable pour stocker le nom du visiteur actuel. Comment nommeriez-vous cette variable ?
+*/
+
+let bluearth = "Terre";
+
+let currentuser = "Robert";
+
+
+
