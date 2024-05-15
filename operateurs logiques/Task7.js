360532e383aa69e206421378c9e57e0c6be85e9c
Fethisensei <fetam62@gmail.com>
2024-05-15 15:52:46 +0200

    refactor(Task7.js):add file

---
+++ operateurs logiques/Task7.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*
+Ecrivez une condition if pour vérifier que l’age n’est PAS compris entre 14 et 90 ans inclus.
+
+Créez deux variantes: la première utilisant NOT !, La seconde – sans ce dernier.
+*/
+let age;
+
+if (age < 14 || age > 90) {
+
+}
+
