c59f3ad98b9dc27fcd26da4c96e262ff098afcb0
Fethisensei <fetam62@gmail.com>
2024-05-15 15:07:21 +0200

    docs(task4.js):add file

---
+++ operateurs logiques/Task4.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/*
+Qu’est-ce que le code ci-dessous va sortir ?
+
+alert( alert(1) || 2 || alert(3) );
+*/
+
+alert(alert(1) || 2 || alert(3)); // une alterte avec un 1 et ensuite une autre avec un 2, on s'arrêt là.
