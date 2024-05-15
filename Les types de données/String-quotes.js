665d74933703a88fe97902d8747c25a297d5ef9e
Fethisensei <fetam62@gmail.com>
2024-05-15 11:31:29 +0200

    docs(Les-types-de-données/Strings-quotes.js):add folder and file

---
+++ Les types de données/String-quotes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+Quelle est la sortie du script ?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+
+*/
+
+let name = "Ilya";
+
+console.log(`hello ${1}`);
+
+console.log(`hello ${"name"}`);
+
+console.log(`hello ${name}`);
