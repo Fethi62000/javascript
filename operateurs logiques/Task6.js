a0d59859f21c2695ab5ab272605c1198620c1610
Fethisensei <fetam62@gmail.com>
2024-05-15 15:34:15 +0200

    docs(Task6.js):add file

---
+++ operateurs logiques/Task6.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*
+Ecrivez une condition "if" pour vérifier que l’age est compris entre 14 et 90 ans inclus.
+
+“Inclus” signifie que l’age peut atteindre les 14 ou 90 ans.
+*/
+
+
+if (age >= 14 && age <= 90); {
+
+}
