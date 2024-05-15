6267440bf831ef37fac5ff66d2c5b0168c6640f6
Fethisensei <fetam62@gmail.com>
2024-05-15 12:05:39 +0200

    docs(Opérateurs-de-base/the-profix-prefix-forms.js):add folder and file

---
+++ Opérateurs-de-base/The postfix-prefix-forms.js
@@ -0,0 +1,15 @@
+"use strict"
+
+/*
+What are the final values of all variables a, b, c and d after the code below?
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+*/
+
+let a = 1, b = 1;
+
+let c = ++a; // 2
+let d = b++; // 1
+
