e4d108a6c2819c83f8397236636f291b383dbd9f
Fethisensei <fetam62@gmail.com>
2024-05-15 23:03:15 +0200

    refactor(quelle-valeur-affiche-la-boucle-while.js):add loops in file

--- Boucles-while-et-for/quelle-valeur-affiche-la-boucle-while.js
+++ Boucles-while-et-for/quelle-valeur-affiche-la-boucle-while.js
@@ -16,4 +16,14 @@ let i = 0;
 while (i++ < 5) alert( i );
 */
 
+let i = 0;
+while (++i < 5) alert(i);
+
+
+
+while (i++ < 5) alert(i);
+
+// logiquement le prefix, renvoie à la valeur après l'incrémentation, donc c'est de 1 à 4.
+// le postfix renvoie à la valeur avant l'incrémentation, donc on commence à partir de 0 à 4.
+// Mais nous avons vu en cours que les deux boucles affichent les même valeur et commence par 0 toutes les deux
 
