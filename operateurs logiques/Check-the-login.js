6220713306d4ae7e1a2215e3ae52a648e4fcb7bc
Fethisensei <fetam62@gmail.com>
2024-05-15 16:33:15 +0200

    docs(operateurs logiques/check-the-login.js): add file

---
+++ operateurs logiques/Check-the-login.js
@@ -0,0 +1,14 @@
+"use strict"
+
+/*
+Écrivez le code qui demande une connexion avec prompt.
+
+Si le visiteur entre "Admin", puis prompt pour un mot de passe, si l’entrée est une ligne vide ou Esc – affichez “Canceled”, s’il s’agit d’une autre chaîne de caractères – alors affichez “I don’t know you”.
+
+Le mot de passe est vérifié comme suit :
+
+    S’il est égal à “TheMaster”, alors affichez “Welcome!”,
+    Une autre chaînede caractères – affichez “Wrong password”,
+    Pour une chaîne de caractères vide ou une entrée annulée, affichez “Canceled”.
+*/
+
