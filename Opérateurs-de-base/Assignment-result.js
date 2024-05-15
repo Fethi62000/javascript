b27848e2596ca636a700fda8c39db4aa086553d0
Fethisensei <fetam62@gmail.com>
2024-05-15 12:19:12 +0200

    docs(Opérateurs-de-base/Assignment-result.js): add file in folder)

---
+++ Opérateurs-de-base/Assignment-result.js
@@ -0,0 +1,14 @@
+"use strict"
+
+/*
+Quelles sont les valeurs de a et x après le code ci-dessous ?
+
+let a = 2;
+
+let x = 1 + (a *= 2);
+
+*/
+
+let a = 2; // a = 2 ici
+
+let x = 1 + (a *= 2); // a = (2 * 2) = 4 & x = 1 + 4 = 5
