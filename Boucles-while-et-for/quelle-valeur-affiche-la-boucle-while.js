674fba12c10a2a37c14d5386c71afe980ac3eaf8
Fethisensei <fetam62@gmail.com>
2024-05-15 22:52:51 +0200

    docs(quelle-valeur-affiche-la-boucle-while.js): add file

---
+++ Boucles-while-et-for/quelle-valeur-affiche-la-boucle-while.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+A votre avis, quelles sont les valeurs affichées pour chaque boucle ? Notez-les puis comparer avec la réponse.
+
+Les deux boucles affichent-elles les mêmes valeurs dans l’alert ou pas ?
+
+    Le préfixe sous forme ++i :
+
+let i = 0;
+while (++i < 5) alert( i );
+
+Le postfixe sous forme i++ :
+
+let i = 0;
+while (i++ < 5) alert( i );
+*/
+
+
