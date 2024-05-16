cc5573648a96d11793b07da521592fdba70d8e6d
Fethisensei <fetam62@gmail.com>
2024-05-16 09:57:08 +0200

    refactor(Output-even-numbers-loop.js)

---
+++ Boucles-while-et-for/Output-even-numbers-loop.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+Utilisez la boucle for pour afficher les nombres pairs de 2 à 10.
+*/
+
+
+
+for (let i = 2; i <= 10; i++) {
+
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+
+}
+
