9b730037c4df0c50d38f02d53adf384e8b9459b9
Fethisensei <fetam62@gmail.com>
2024-05-16 11:29:14 +0200

    refactor(Repeat-until-input-correct.js): add while loop

--- Boucles-while-et-for/Repeat-until-input -correct.js
+++ Boucles-while-et-for/Repeat-until-input -correct.js
@@ -8,3 +8,14 @@ La boucle doit demander un numéro jusqu’à ce que le visiteur saisisse un nom
 Ici, nous pouvons supposer que le visiteur ne saisit que des chiffres. Il n’est pas nécessaire de mettre en œuvre un traitement spécial pour une entrée non numérique dans cette tâche.
 */
 
+let i = +prompt("écrivez un nombre supérieur à 100");
+
+while (i) {
+    if (i > 100) {
+        console.log(i);
+    } else {
+        console.log("saisir à nouveau");
+
+    }
+
+}
