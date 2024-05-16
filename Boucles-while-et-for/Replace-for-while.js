a58cf66e730c5483fcdd724f8c7424270cd5beb5
Fethisensei <fetam62@gmail.com>
2024-05-16 10:32:52 +0200

    refactor(Replace-for-while.js): add the while loop

---
+++ Boucles-while-et-for/Replace-for-while.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+Réécrivez le code en modifiant la boucle for en while sans modifier son comportement (la sortie doit rester la même).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+*/
+
+let i = 0;
+
+while (i < 3) {
+    console.log(`number ${i}!`);
+    i++;
+}
