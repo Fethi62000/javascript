a031f81b0042497c2e6c172b3873a2957568fcc4
Fethisensei <fetam62@gmail.com>
2024-05-16 22:57:51 +0200

    docs(fonction): add file

---
+++ Fonction/Is_"else"_required.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+La fonction suivante renvoie true si le paramètre age est supérieur à 18.
+
+Sinon, il demande une confirmation et renvoie son résultat :
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('Did parents allow you?');
+  }
+}
+
+La fonction fonctionnera-t-elle différemment si else est supprimé ?
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  }
+  // ...
+  return confirm('Did parents allow you?');
+}
+
+Existe-t-il une différence dans le comportement de ces deux variantes ?
+*/
+
