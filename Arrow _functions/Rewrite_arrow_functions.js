f6086a693e96d3ae04fa40167649e5e90ceb862a
Fethisensei <fetam62@gmail.com>
2024-05-17 10:52:58 +0200

    docs(Rewrite_arrow_functions.js): add file

---
+++ Arrow _functions/Rewrite_arrow_functions.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+Remplacez les expressions de fonction par des fonctions fléchées dans le code ci-dessous :
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+*/
